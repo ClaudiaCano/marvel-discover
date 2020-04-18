694485189688dbedd9dd7ba3d69bd9ba8b556247
ClaudiaCano <claudiacanolucena@outlook.com>
2020-04-18 18:06:44 +0200

    object StarWars
    objeto JS del comic StarWars

--- pages/Comic.js
+++ pages/Comic.js
@@ -10,17 +10,36 @@ import ComicBar from "../components/ComicBar";
 import BackSvg from "../assets/back.svg";
 import BtnLeido from "../components/BtnLeido";
 
+const StarWars  = {
+    title: "Star Wars (2015) #1",
+    author: "Jason Aaron",
+    date: "14 enero, 2015",
+    characters:{
+        leia:{
+            character: "Princesa Leia",
+            completeName: "Leia Organa",
+            photo: "../assets/Leia.jpg",
+        },
+        darthVader:{
+            character: "Darth Vader",
+            completeName: ""
+        }
+    },
+    synopsis: "THE GREATEST SPACE ADVENTURE OF ALL TIME RETURNS TO MARVEL! Luke Skywalker and the ragtag band of rebels fighting against the Galactic Empire are fresh off their biggest victory yet-the destruction of the massive battle station known as the Death Star. But the Empire's not toppled yet! Join Luke along with Princess Leia, smugglers Han Solo and Chewbacca, droids C-3PO and R2-D2 and the rest of the Rebel Alliance as they strike out for freedom against the evil forces of Darth Vader and his master, the Emperor. Written by Jason Aaron (Original Sin, Thor: God of Thunder) and with art by John Cassaday (Astonishing X-Men, Uncanny Avengers), this is the Star Wars saga as only Marvel Comics could make it!",           
+    cover: "https://i.annihil.us/u/prod/marvel/i/mg/1/20/567083a7957b5/clean.jpg"
+}
+
 export default class Comic extends React.Component {
     render() {
         return (
             <View>
                 <BackSvg style={styles.backIcon} />
                 <ScrollView>
-                    <ComicHeader Cover="https://i.annihil.us/u/prod/marvel/i/mg/1/20/567083a7957b5/clean.jpg" />
-                    <ComicTitle Title="Star Wars (2015) #1" />
-                    <ComicDetails Name="Jason Aaron" Date="14 enero, 2015" />
-                    <ComicPersonajes />
-                    <ComicSynopsis Synopsis="THE GREATEST SPACE ADVENTURE OF ALL TIME RETURNS TO MARVEL! Luke Skywalker and the ragtag band of rebels fighting against the Galactic Empire are fresh off their biggest victory yet-the destruction of the massive battle station known as the Death Star. But the Empire's not toppled yet! Join Luke along with Princess Leia, smugglers Han Solo and Chewbacca, droids C-3PO and R2-D2 and the rest of the Rebel Alliance as they strike out for freedom against the evil forces of Darth Vader and his master, the Emperor. Written by Jason Aaron (Original Sin, Thor: God of Thunder) and with art by John Cassaday (Astonishing X-Men, Uncanny Avengers), this is the Star Wars saga as only Marvel Comics could make it!" />
+                    <ComicHeader Cover = {StarWars.cover} />
+                    <ComicTitle Title = {StarWars.title} />
+                    <ComicDetails Name = {StarWars.author} Date = {StarWars.date} />
+                    <ComicPersonajes Characters = {StarWars.characters} />
+                    <ComicSynopsis Synopsis = {StarWars.synopsis} />
                 </ScrollView>
                 <ComicBar />
                 <BtnLeido />
