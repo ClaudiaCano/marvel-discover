e1c4eb069898084dce46678678c6a322e403776f
ClaudiaCano <claudiacanolucena@outlook.com>
2020-04-23 18:09:47 +0200

    Fonts en ComicSynopsis

--- pages/Comic.js
+++ pages/Comic.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { StyleSheet, ScrollView, View } from "react-native";
-import * as Font from "expo-font";
 import ComicHeader from "../components/ComicHeader";
 import ComicTitle from "../components/ComicTitle";
 import ComicDetails from "../components/ComicDetails";
@@ -10,39 +9,41 @@ import ComicBar from "../components/ComicBar";
 import BackSvg from "../assets/back.svg";
 import BtnLeido from "../components/BtnLeido";
 
-const StarWars  = {
+const StarWars = {
     title: "Star Wars (2015) #1",
     author: "Jason Aaron",
     date: "14 enero, 2015",
-    characters:{
-        leia:{
+    characters: {
+        leia: {
             character: "Princesa Leia",
             completeName: "Leia Organa",
             photo: "../assets/Leia.jpg",
         },
-        darthVader:{
+        darthVader: {
             character: "Darth Vader",
-            completeName: ""
-        }
+            completeName: "",
+        },
     },
-    synopsis: "THE GREATEST SPACE ADVENTURE OF ALL TIME RETURNS TO MARVEL! Luke Skywalker and the ragtag band of rebels fighting against the Galactic Empire are fresh off their biggest victory yet-the destruction of the massive battle station known as the Death Star. But the Empire's not toppled yet! Join Luke along with Princess Leia, smugglers Han Solo and Chewbacca, droids C-3PO and R2-D2 and the rest of the Rebel Alliance as they strike out for freedom against the evil forces of Darth Vader and his master, the Emperor. Written by Jason Aaron (Original Sin, Thor: God of Thunder) and with art by John Cassaday (Astonishing X-Men, Uncanny Avengers), this is the Star Wars saga as only Marvel Comics could make it!",           
-    cover: "https://i.annihil.us/u/prod/marvel/i/mg/1/20/567083a7957b5/clean.jpg"
-}
+    synopsis:
+        "THE GREATEST SPACE ADVENTURE OF ALL TIME RETURNS TO MARVEL! Luke Skywalker and the ragtag band of rebels fighting against the Galactic Empire are fresh off their biggest victory yet-the destruction of the massive battle station known as the Death Star. But the Empire's not toppled yet! Join Luke along with Princess Leia, smugglers Han Solo and Chewbacca, droids C-3PO and R2-D2 and the rest of the Rebel Alliance as they strike out for freedom against the evil forces of Darth Vader and his master, the Emperor. Written by Jason Aaron (Original Sin, Thor: God of Thunder) and with art by John Cassaday (Astonishing X-Men, Uncanny Avengers), this is the Star Wars saga as only Marvel Comics could make it!",
+    cover:
+        "https://i.annihil.us/u/prod/marvel/i/mg/1/20/567083a7957b5/clean.jpg",
+};
 
 export default class Comic extends React.Component {
     render() {
         return (
             <View>
                 <BackSvg style={styles.backIcon} />
-                <ScrollView>
-                    <ComicHeader Cover = {StarWars.cover} />
-                    <ComicTitle Title = {StarWars.title} />
-                    <ComicDetails Name = {StarWars.author} Date = {StarWars.date} />
-                    <ComicPersonajes Characters = {StarWars.characters} />
-                    <ComicSynopsis Synopsis = {StarWars.synopsis} />
+                <ScrollView showsVerticalScrollIndicator={false}>
+                    <ComicHeader Cover={StarWars.cover} />
+                    <ComicTitle Title={StarWars.title} />
+                    <ComicDetails Name={StarWars.author} Date={StarWars.date} />
+                    <ComicPersonajes Characters={StarWars.characters} />
+                    <ComicSynopsis Synopsis={StarWars.synopsis} />
                 </ScrollView>
 
-                <ComicBar /> 
+                <ComicBar />
                 <BtnLeido />
             </View>
         );
@@ -54,6 +55,6 @@ const styles = StyleSheet.create({
         position: "absolute",
         top: 10,
         left: 20,
-        zIndex: 2
+        zIndex: 2,
     },
 });
