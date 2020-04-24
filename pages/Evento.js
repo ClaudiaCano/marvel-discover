a60c7e0e45d0ab49eba29bb012d824559d7bd867
Claudia Abate <claudia.abate98@gmail.com>
2020-04-24 23:28:22 +0200

    Fondo Evento

--- pages/Evento.js
+++ pages/Evento.js
@@ -1,10 +1,10 @@
 import React from "react";
 import {
-    StyleSheet,
-    ScrollView,
-    View,
-    Dimensions,
-    TouchableHighlight,
+  StyleSheet,
+  ScrollView,
+  View,
+  Dimensions,
+  TouchableHighlight,
 } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 
@@ -13,105 +13,111 @@ import EventTitle from "../components/EventTitle";
 import CardComic from "../components/CardComicEvent";
 import Details from "../components/Details";
 import BackIcon from "../assets/backblack.svg";
+import AppBarBackground from "../components/AppBarBackground";
 
 /*------------------------ OBJECTS ------------------------ */
 const data = [
-    {
-        imageUrl:
-            "https://i.annihil.us/u/prod/marvel/i/mg/1/20/567083a7957b5/clean.jpg",
-    },
-    {
-        imageUrl:
-            "https://i.annihil.us/u/prod/marvel/i/mg/3/a0/5670864d95ab0/clean.jpg",
-    },
-    {
-        imageUrl:
-            "https://i.annihil.us/u/prod/marvel/i/mg/3/50/567084ad58a03/clean.jpg",
-    },
-    {
-        imageUrl:
-            "https://i.annihil.us/u/prod/marvel/i/mg/3/20/5519439c0b32c/clean.jpg",
-    },
-    {
-        imageUrl:
-            "https://i.annihil.us/u/prod/marvel/i/mg/e/d0/54a2fda8ace40/clean.jpg",
-    },
-    {
-        imageUrl:
-            "https://i.annihil.us/u/prod/marvel/i/mg/6/90/5536b03673fdf/clean.jpg",
-    },
+  {
+    imageUrl:
+      "https://i.annihil.us/u/prod/marvel/i/mg/1/20/567083a7957b5/clean.jpg",
+  },
+  {
+    imageUrl:
+      "https://i.annihil.us/u/prod/marvel/i/mg/3/a0/5670864d95ab0/clean.jpg",
+  },
+  {
+    imageUrl:
+      "https://i.annihil.us/u/prod/marvel/i/mg/3/50/567084ad58a03/clean.jpg",
+  },
+  {
+    imageUrl:
+      "https://i.annihil.us/u/prod/marvel/i/mg/3/20/5519439c0b32c/clean.jpg",
+  },
+  {
+    imageUrl:
+      "https://i.annihil.us/u/prod/marvel/i/mg/e/d0/54a2fda8ace40/clean.jpg",
+  },
+  {
+    imageUrl:
+      "https://i.annihil.us/u/prod/marvel/i/mg/6/90/5536b03673fdf/clean.jpg",
+  },
 ];
 const StarWars = {
-    title: "Star Wars (2015)",
-    description:
-        "A long time ago in a galaxy far, far away, the adventures of Luke Skywalker, Han Solo, Princess, Leia, Darth Vader and more continue! Set following the events of Episode IV: A New Hope, Marvel's ongoing Star Wars comics reveal new stories featuring these classic characters!",
-    creators:
-        "Jason Aaron, John Cassaday, Kieron Gillen, Salvador Larroca, Adi Granov Mark Waid, Terry Dodson, Greg Weisman, Mark Brooks, Pepe Larraz, Stuart Immonen.",
+  title: "Star Wars (2015)",
+  description:
+    "A long time ago in a galaxy far, far away, the adventures of Luke Skywalker, Han Solo, Princess, Leia, Darth Vader and more continue! Set following the events of Episode IV: A New Hope, Marvel's ongoing Star Wars comics reveal new stories featuring these classic characters!",
+  creators:
+    "Jason Aaron, John Cassaday, Kieron Gillen, Salvador Larroca, Adi Granov Mark Waid, Terry Dodson, Greg Weisman, Mark Brooks, Pepe Larraz, Stuart Immonen.",
 };
 
 /*------------------------ PAGE ------------------------ */
 export default class Evento extends React.Component {
-    constructor(props) {
-        super(props);
-        this.buttonPress = this.buttonPress.bind(this);
-        this.buttonBack = this.buttonBack.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.buttonPress = this.buttonPress.bind(this);
+    this.buttonBack = this.buttonBack.bind(this);
+  }
 
-    buttonPress() {
-        console.log("comic");
-        this.props.navigation.navigate("comic");
-    }
-    buttonBack() {
-        console.log("back");
-        this.props.navigation.goBack();
-    }
+  buttonPress() {
+    console.log("comic");
+    this.props.navigation.navigate("comic");
+  }
+  buttonBack() {
+    console.log("back");
+    this.props.navigation.goBack();
+  }
 
-    render() {
-        return (
-            <View>
-                <LinearGradient
-                    colors={["#FAF2FF", "#fff0"]}
-                    style={styles.gradient}
-                />
-                <BackIcon style={styles.backIcon} />
-                <TouchableHighlight onPress={this.buttonBack}>
-                    
-                    
-                <EventTitle Title={StarWars.title} />
-                </TouchableHighlight>
-                <ScrollView>
-                    <CardComic Data={data}/>
-                    <Details
-                        Title="Descripción"
-                        Description={StarWars.description}
-                    />
-                    <Details
-                        Title="Creadores"
-                        Description={StarWars.creators}
-                    />
-                    <View style={styles.sizedbox} />
-                </ScrollView>
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View>
+        <LinearGradient
+          colors={["white", "white", "#B895C8"]}
+          style={styles.gradient}
+        />
+        <LinearGradient
+          colors={["#FAF2FF", "#fff0"]}
+          style={styles.gradientsup}
+        />
+        <BackIcon style={styles.backIcon} />
+        <TouchableHighlight onPress={this.buttonBack}>
+          <EventTitle Title={StarWars.title} />
+        </TouchableHighlight>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <CardComic Data={data} />
+          <Details Title="Descripción" Description={StarWars.description} />
+          <Details Title="Creadores" Description={StarWars.creators} />
+          <View style={styles.sizedbox} />
+        </ScrollView>
+        <AppBarBackground />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    gradient: {
-        position: "absolute",
-        left: 0,
-        right: 0,
-        top: 0,
-        height: Dimensions.get("window").height,
-        zIndex: -1,
-    },
-    backIcon: {
-        position: "absolute",
-        top: 38,
-        left: 30,
-        zIndex: 2,
-    },
-    sizedbox: {
-        height: 150,
-    },
+  gradient: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 0,
+    height: Dimensions.get("window").height,
+    zIndex: -1,
+  },
+  gradientsup: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 0,
+    height: Dimensions.get("window").height,
+    zIndex: -1,
+  },
+  backIcon: {
+    position: "absolute",
+    top: 38,
+    left: 30,
+    zIndex: 2,
+  },
+  sizedbox: {
+    height: 150,
+  },
 });
